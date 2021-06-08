0d39f2a8a3f7cb092321f7196af22a735cec37d2
zhongweipeng <zhongwp@shushangyun.com>
2021-06-08 14:32:33 +0800

    feat:

---
+++ bin/node-echo.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+const program = require('commander'); // 命令行工具
+const argv = require('argv'),
+    echo = require('../lib/echo');
+const inquirer = require('inquirer');
+const verifyNodeVersion = require('../lib/utils/verifyNodeVersion');
+const TEMPLATE_ENUM = require('../lib/template/templateEnum.json');
+const { engines, name, version } = require('../package.json');
+const createTemplate = require('../lib/template/createTemplate')
+
+/** 校验所需node版本 */
+verifyNodeVersion(engines.node, name);
+
+program.version(version, '-v, --version') // 输出版本信息
+.usage('<command> [options]') // 如何使用
+
+program.command('create')
+.description('创建一个项目模板')
+.action(async (cmd) => {
+  const { template } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'template',
+      message: '选择要使用的模板',
+      choices: Object.keys(TEMPLATE_ENUM)
+    }
+  ])
+  createTemplate(template)
+})
+
+program.parse(process.argv); // 把命令行参数传给commander解析
+    
+console.log(echo(argv.run().targets.join(' ')));
